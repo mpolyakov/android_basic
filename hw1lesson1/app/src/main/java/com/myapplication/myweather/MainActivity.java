b3855048c238b7a52ebce49bf0e0d1c9b5dca341
mpolyakov <polyakov4@yandex.ru>
2019-09-19 01:24:59 +0300

    добавил Toast's информирующие о состоянии жизненного цикла активити. Логирование с помощью Log.d. И сохранение и восстановление переменной tempStr.

--- hw1lesson1/app/src/main/java/com/myapplication/myweather/MainActivity.java
+++ hw1lesson1/app/src/main/java/com/myapplication/myweather/MainActivity.java
@@ -1,14 +1,98 @@
 package com.myapplication.myweather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "myLogs";
+    private TextView temperature;
+    private String tempString = "+27";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String instanceState;
+        if(savedInstanceState == null){
+            Log.d(TAG, "Первый запуск!");
+            instanceState = "Первый запуск!";
+        } else {
+            Log.d(TAG, "Повторный запуск!");
+            instanceState = "Повторный запуск!";
+        }
+        Toast.makeText(getApplicationContext(), instanceState, Toast.LENGTH_SHORT).show();
+
+        temperature = findViewById(R.id.textView);
+        temperature.setText(tempString);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        Toast.makeText(getApplicationContext(), "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+        Toast.makeText(getApplicationContext(), "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+        Toast.makeText(getApplicationContext(), "onDestroy", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+        Toast.makeText(getApplicationContext(), "onSaveInstanceState", Toast.LENGTH_SHORT).show();
+
+        outState.putString("Temperature", tempString);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+        Toast.makeText(getApplicationContext(), "onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        Toast.makeText(getApplicationContext(), "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+        Toast.makeText(getApplicationContext(), "onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected  void onRestoreInstanceState(@NonNull Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+
+        tempString = savedInstanceState.getString("Temperature");
+        temperature.setText(tempString);
+        Log.d(TAG, "onRestoreInstanceState");
+        Toast.makeText(getApplicationContext(), "onRestoreInstanceState", Toast.LENGTH_SHORT).show();
+
     }
 }
