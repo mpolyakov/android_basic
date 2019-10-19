427f4491662fd9ed54f9a407f9246b71c6c230f5
mpolyakov <polyakov4@yandex.ru>
2019-10-19 21:50:29 +0300

    добавил список recyclerView для отображения прогноза погоды на 7 дней. Список во фрагменте. Для портретной ориентации - прокрутка горизонтальная, для ландшафтной  - прокрутка вертикальная.

--- hw1lesson1/app/src/main/java/com/myapplication/myweather/MainActivity.java
+++ hw1lesson1/app/src/main/java/com/myapplication/myweather/MainActivity.java
@@ -2,8 +2,11 @@ package com.myapplication.myweather_copy;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,12 +15,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "myLogs";
     private TextView temperature;
     private String tempString = "+27";
     static final private int CHOOSE_CITY = 0;
+    private ArrayList<String> mDays = new ArrayList<>();
+    private ArrayList<String> mForecTemperature = new ArrayList<>();
+    private ArrayList<Integer> mImagesId = new ArrayList<Integer>();
 
 
     @Override
@@ -36,6 +44,21 @@ public class MainActivity extends AppCompatActivity {
 
         temperature = findViewById(R.id.textView);
         temperature.setText(tempString);
+
+        initFillingRecycleArrays();
+
+        ImageView imageViewBrow = findViewById(R.id.imageViewInternet);
+        imageViewBrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://gismeteo.ru";
+                Uri uri = Uri.parse(url);
+
+                Intent browser = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(browser);
+
+            }
+        });
 
 
     }
@@ -83,8 +106,6 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         Log.d(TAG, "onResume");
         Toast.makeText(getApplicationContext(), "onResume", Toast.LENGTH_SHORT).show();
-
-
     }
 
     @Override
@@ -92,31 +113,20 @@ public class MainActivity extends AppCompatActivity {
         super.onRestart();
         Log.d(TAG, "onRestart");
         Toast.makeText(getApplicationContext(), "onRestart", Toast.LENGTH_SHORT).show();
-
-//        if (chkbx){
-//            ImageView iv=(ImageView) findViewById(R.id.imageView);
-//            iv.setVisibility(View.VISIBLE);
-//        }
-
     }
 
     @Override
     protected  void onRestoreInstanceState(@NonNull Bundle savedInstanceState){
         super.onRestoreInstanceState(savedInstanceState);
-
         tempString = savedInstanceState.getString("Temperature");
         temperature.setText(tempString);
         Log.d(TAG, "onRestoreInstanceState");
         Toast.makeText(getApplicationContext(), "onRestoreInstanceState", Toast.LENGTH_SHORT).show();
-
     }
 
     public void onClickChangeCity(View view) {
-
         Intent intentForResult = new Intent(MainActivity.this, ChangeCityActivity.class);
         startActivityForResult(intentForResult, CHOOSE_CITY);
-
-
     }
 
     @Override
@@ -133,6 +143,68 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void initFillingRecycleArrays(){
+        mDays.add("ПН");
+        mDays.add("ВТ");
+        mDays.add("СР");
+        mDays.add("ЧТ");
+        mDays.add("ПТ");
+        mDays.add("СБ");
+        mDays.add("ВС");
+
+        mForecTemperature.add("+18");
+        mForecTemperature.add("+19");
+        mForecTemperature.add("+20");
+        mForecTemperature.add("+21");
+        mForecTemperature.add("+22");
+        mForecTemperature.add("+23");
+        mForecTemperature.add("+24");
+
+        mImagesId.add(R.drawable.sun240);
+        mImagesId.add(R.drawable.snow192);
+        mImagesId.add(R.drawable.sun240);
+        mImagesId.add(R.drawable.storm100_f);
+        mImagesId.add(R.drawable.rain100_f);
+        mImagesId.add(R.drawable.cloudly200_f);
+        mImagesId.add(R.drawable.sun240);
+
+        initRecyclerView();
+
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mDays, mForecTemperature, mImagesId, this);
+        recyclerView.setAdapter(adapter);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        }
+        else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        }
+
+    }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
